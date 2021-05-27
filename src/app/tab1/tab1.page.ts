5fac4cd6b62c61e199225a83d03cd23e26b687ed
jussimar <jussimar@jussimarleal.com.br>
2021-05-27 15:14:56 -0300

    criando interface e criando relação de filmes no ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from './../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'RoboCop (1987)',
+      lancamento: '08/10/1987',
+      duracao:'1h 42m',
+      classificacao: 72,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fR0lfsB7YRNy2NQo9Ii7CPnYp31.jpg',
+      generos:['Ação', 'Thriller', 'Ficção','científica']
+    },
+    {
+      nome: 'RoboCop (1987) 2',
+      lancamento: '08/10/1987',
+      duracao:'1h 42m',
+      classificacao: 72,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fR0lfsB7YRNy2NQo9Ii7CPnYp31.jpg',
+      generos:['Ação', 'Thriller', 'Ficção','científica']
+    }
+  ];
 
   constructor(
     public alertController: AlertController,
